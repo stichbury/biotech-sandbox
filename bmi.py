6c975786d716d256ca1fcd7df1d12fa4a1743a15
Jo Stichbury <jo.stichbury@mac.com>
2017-07-05 15:00:27 +0100

    Update code and remove some NaN values plus one typo in dataset

--- bmi.py
+++ bmi.py
@@ -2,8 +2,7 @@
 # Import numpy
 import numpy as np
 
-# Build an array of my recent weigh in data
-
+# Build an array of my recent weigh-in data
 np_weight = np.array([64.4, 64.7, 64.5, 64.4, 64.1, 63.9, 65.5, 64.6, 64.8, 64.8, 64.9, 65.0, 64.1, 63.7])
 
 # Calculate the BMI against each of these
@@ -13,39 +12,41 @@ np_bmi = np_weight / height ** 2
 
 print (np_bmi)
 
-import csv
+# -------------------------------------------
+# TO DO - work out how to manage NaN values and use original responses.csv (so I can put this in a Kaggle notebook)
+# What I've done to the data so far: remove NaN values for age, height and weight, spending on healthy eating. Fixed one value of height from 62 to 162 (probably was a typo, otherwise an outlier)
 
-with open('responses-noNAN.csv') as csvfile:
-    next(csvfile)
-    readCSV = csv.reader(csvfile, delimiter=',')
-    next(readCSV)
+np_responses = np.genfromtxt("responses-noNAN.csv", skip_header = 1, delimiter = ",", dtype = "U75")
+np_student_weights = np_responses[:,-8]
+np_student_heights = np_responses[:,-9]
 
-    student_weights = []
-    student_heights = []
-
-    for row in readCSV:
-    	student_heights.append(row[-9])
-    	student_weights.append(row[-8])
-
-
-np_student_heights = np.array(student_heights)
 np_student_heights = np_student_heights.astype(np.int)
-
-np_student_weights = np.array(student_weights)
 np_student_weights = np_student_weights.astype(np.int)
 
+
 np_student_bmi = 10000 * np_student_weights/np_student_heights ** 2
-print(np_student_bmi)
-	
+print(np_student_bmi)	
+
+# Visualisation of BMI with age, gender, spending on healthy eating etc
+
+import matplotlib.pyplot as plt
+
+# First, age
+
+np_student_ages = np_responses[:,-10]
+np_student_ages = np_student_ages.astype(np.int)
+
+plt.scatter(np_student_ages, np_student_bmi)
+plt.show()
+
+# Now bmi against spending on healthy eating
+np_student_healthy_spending = np_responses[:,-11]
+np_student_healthy_spending = np_student_healthy_spending.astype(np.int)
+
+plt.scatter(np_student_healthy_spending, np_student_bmi)
+plt.show()
 
 
-# Correlation of BMI with age, gender, spending on healthy eating etc
-
-print(np.shape(np_student_bmi))
 
 
 
-
-
-
-#genfromtxt("world_alcohol.csv", skip_header = 1, delimiter = ",", dtype = "U75")
